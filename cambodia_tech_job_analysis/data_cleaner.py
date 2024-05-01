bfb75f6c9c508479a2f9cc5e9142610c5e87332a
Votana SREY <srey.votana@ababank.com>
2024-05-01 14:35:35 +0700

    Updated script and data visualizer

--- cambodia_tech_job_analysis/data_cleaner.py
+++ cambodia_tech_job_analysis/data_cleaner.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-path = "datasets\clean_dataset.csv"
+path = "datasets\primary_dataset.csv"
 file = pd.read_csv(path)
 
 ICT = ["Computer Science",
@@ -34,7 +34,6 @@ ICT = ["Computer Science",
 "Telecoms and Networking",
 ]
 
-# Remove Non-ICT data from dataset
 for index,row in file.iterrows():
     found = False
     data = file.loc[index:index,"Major 1": "Major 6"]
@@ -46,20 +45,16 @@ for index,row in file.iterrows():
     if found == False: 
         file = file.drop(labels=index,axis=0)
 
-# Replace Non-ICT major in ICT dataset with empty string
 for index,row in file.iterrows():
     data = file.loc[index:index,"Major 1": "Major 6"]
     for item in data:
         if data[item][index] not in ICT:
             file.loc[index:index,item:item] = ""
 
-# remove index
 file = file.reset_index(drop=True)
 
-# drop column that doesn't have anything
 for item in ["Major 1","Major 2","Major 3","Major 4","Major 5","Major 6"]:
     if (file[item].values == '').sum() == len(file):
         file = file.drop(columns=item)
 
-# write to csv file
-file.to_csv("datasets/clean_ict.csv",index=False)
+file.to_csv("datasets/dataset_v1.csv",index=False)
--- cambodia_tech_job_analysis/data_scraper.py
+++ cambodia_tech_job_analysis/data_scraper.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-
 import os
 import json
 from bs4 import BeautifulSoup
